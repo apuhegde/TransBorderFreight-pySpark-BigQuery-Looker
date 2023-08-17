403ca9af8f23025112e17764fc9aa7c0adb71c7a
Apurva Hegde <ahegde@Apurvas-Laptop.local>
2023-08-17 07:48:34 -0500

    Added code to clone git repo

--- Terraform/VM_provisioning.sh
+++ Terraform/VM_provisioning.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+git clone https://github.com/apuhegde/TransBorderFreight-pySpark-BigQuery-Looker.git
+
 sudo apt install apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=$(dpkg --print-architecture)] https://download.docker.com/linux/ubuntu `lsb_release -cs` test"
