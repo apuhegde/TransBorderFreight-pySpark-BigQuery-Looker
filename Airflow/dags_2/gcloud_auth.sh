973ef16416e20a17cef13ca817f3da30f778ccd5
Apurva Hegde <ahegde@Apurvas-Laptop.local>
2023-08-28 10:30:55 -0500

    dir cleanup and purging

--- Airflow/dags_2/gcloud_auth.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-gcloud auth activate-service-account terraform@trans-border-freights-394419.iam.gserviceaccount.com --key-file /.google/credentials/google_credentials.json
--- gcloud_setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#gcloud auth activate-service-account terraform@trans-border-freights-394419.iam.gserviceaccount.com --key-file /home/ahegde/.google/credentials/google_credentials.json
-
-gsutil cp ~/TransBorderFreight-pySpark-BigQuery-Looker/02a_transform_consolidate.py gs://tbf-analysis-terraform_trans-border-freights-394419/code/02a_transform_consolidate.py
-gsutil cp ~/TransBorderFreight-pySpark-BigQuery-Looker/02b_transform_joinwithmetadata.py gs://tbf-analysis-terraform_trans-border-freights-394419/code/02b_transform_joinwithmetadata.py
-gsutil cp ~/TransBorderFreight-pySpark-BigQuery-Looker/TransBorderCodes.xls gs://tbf-analysis-terraform_trans-border-freights-394419/metadata/TransBorderCodes.xls
